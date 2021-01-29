f0a777f772d0991020cd10806a2c13c2116db3b7
buzzie-bee <39500959+buzzie-bee@users.noreply.github.com>
2021-01-29 16:04:36 +0100

    Created static calendar that renders dates of flights.

---
+++ src/features/flights/CalendarCard.tsx
@@ -0,0 +1,131 @@
+import {
+  Badge,
+  BadgeOrigin,
+  IconButton,
+  // makeStyles,
+  // Theme,
+} from '@material-ui/core';
+import { DatePicker } from '@material-ui/pickers';
+import clsx from 'clsx';
+import React from 'react';
+
+import './CalendarCard.css';
+
+export const CalendarCard = ({
+  start,
+  end,
+}: {
+  start: string;
+  end: string;
+}) => {
+  const startDate = new Date(new Date(start).toDateString());
+  const startDateTime = startDate.getTime();
+  const endDate = new Date(new Date(end).toDateString());
+  const endDateTime = endDate.getTime();
+
+  // const classes = useStyles();
+
+  const renderDay = (
+    date: Date | null,
+    _selectedDate: Date | null,
+    dayInCurrentMonth: boolean
+  ): JSX.Element => {
+    const dayTime = date?.getTime();
+    const todayTime = new Date(new Date().toDateString()).getTime();
+
+    const isInPast = dayTime ? dayTime < todayTime : false;
+    const isRangeStart = dayTime === startDateTime;
+    const isRangeEnd = dayTime === endDateTime;
+    const isInRange = dayTime
+      ? dayTime > startDateTime && dayTime < endDateTime
+      : false;
+
+    // const wrapperClassName = clsx(classes.dayWrapper, {
+    //   [classes.leftHighlight]: isRangeStart,
+    //   [classes.rightHighlight]: isRangeEnd,
+    //   [classes.rangeHighlight]: isInRange,
+    // });
+
+    // const dayClassName = clsx(classes.day, {
+    //   [classes.nonCurrentMonthDay]: !dayInCurrentMonth || isInPast,
+    // });
+    const wrapperClassName = clsx('dayWrapper', {
+      leftHighlight: isRangeStart,
+      rightHighlight: isRangeEnd,
+      rangeHighlight: isInRange,
+    });
+
+    const dayClassName = clsx('day', {
+      nonCurrentMonthDay: !dayInCurrentMonth || isInPast,
+    });
+
+    let badgeContent = '';
+    let badgePosition: BadgeOrigin | undefined = {
+      vertical: 'top',
+      horizontal: 'left',
+    };
+    if (isRangeStart) {
+      badgeContent = '🛫';
+    } else if (isRangeEnd) {
+      badgeContent = '🛬';
+      badgePosition = { vertical: 'top', horizontal: 'right' };
+    }
+
+    return (
+      <Badge badgeContent={badgeContent} anchorOrigin={badgePosition}>
+        <div className={wrapperClassName}>
+          <IconButton className={dayClassName}>
+            <span>{date?.getDate()}</span>
+          </IconButton>
+        </div>
+      </Badge>
+    );
+  };
+
+  return (
+    <DatePicker
+      value={startDate}
+      onChange={() => {}}
+      variant="static"
+      label="FlightDateCalendar"
+      disableToolbar={true}
+      renderDay={renderDay}
+    />
+  );
+};
+
+// const useStyles = makeStyles<Theme>((theme: Theme) => ({
+//   dayWrapper: {
+//     position: 'relative',
+//   },
+//   day: {
+//     width: 36,
+//     height: 36,
+//     fontSize: theme.typography.caption.fontSize,
+//     margin: '0 2px',
+//     color: 'inherit',
+//   },
+//   nonCurrentMonthDay: {
+//     color: theme.palette.text.disabled,
+//   },
+//   highlightNonCurrentMonthDay: {
+//     color: '#676767',
+//   },
+//   leftHighlight: {
+//     background: theme.palette.primary.main,
+//     color: theme.palette.common.white,
+//     borderTopLeftRadius: '25%',
+//     borderBottomLeftRadius: '25%',
+//   },
+//   rightHighlight: {
+//     background: theme.palette.primary.main,
+//     color: theme.palette.common.white,
+//     borderTopRightRadius: '25%',
+//     borderBottomRightRadius: '25%',
+//   },
+//   rangeHighlight: {
+//     background: theme.palette.primary.main,
+//     color: theme.palette.common.white,
+//     opacity: '60%',
+//   },
+// }));
