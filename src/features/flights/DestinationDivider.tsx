73764efb12fb48a1334ece2769d4ea2bd56d4478
buzzie-bee <39500959+buzzie-bee@users.noreply.github.com>
2021-01-29 19:18:10 +0100

    Created destination divider component

---
+++ src/features/flights/DestinationDivider.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import './DestinationDivider.css';
+
+export const DestinationDivider = ({ stops }: { stops: number }) => {
+  const renderStops = () => {
+    if (stops === 0) {
+      return;
+    } else if (stops === 1) {
+      return <div className="dividerStop" />;
+    } else if (stops === 2) {
+      return (
+        <>
+          <div className="dividerStop d33" />
+          <div className="dividerStop d66" />
+        </>
+      );
+    }
+  };
+
+  return (
+    <div className="destinationDivider">
+      <div className="dividerStart" />
+      {renderStops()}
+      <div className="dividerEnd" />
+    </div>
+  );
+};
