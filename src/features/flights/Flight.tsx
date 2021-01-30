e3c2fffb2f5e3370266646596d55e9212a541772
buzzie-bee <39500959+buzzie-bee@users.noreply.github.com>
2021-01-30 18:54:19 +0100

    Cleaned up unused imports and comments

--- src/features/flights/Flight.tsx
+++ src/features/flights/Flight.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Grid, Hidden, Paper, Typography } from '@material-ui/core';
 import { ReferralButton } from './ReferralButton';
 import { CalendarCard } from './CalendarCard';
@@ -75,7 +75,9 @@ export const Flight: React.FC<FlightType> = ({
               >
                 <Grid item>
                   <Logo airlineName={carrier.name} />
-                  <Typography style={{ marginTop: '-16px' }}>
+                  <Typography
+                    style={{ marginTop: '-8px', fontWeight: 'lighter' }}
+                  >
                     {carrier.name}
                   </Typography>
                 </Grid>
@@ -95,7 +97,9 @@ export const Flight: React.FC<FlightType> = ({
           </Grid>
           <Grid container item direction="column" justify="center">
             <Grid item>
-              <Typography style={{ marginTop: '1rem' }}>From</Typography>
+              <Typography style={{ marginTop: '1rem' }}>
+                <span style={{ fontWeight: 'lighter' }}>From:</span>
+              </Typography>
               <Typography
                 style={{
                   fontSize: '5rem',
@@ -147,160 +151,3 @@ export const Flight: React.FC<FlightType> = ({
     </Paper>
   );
 };
-
-// <CalendarCard start={'2021-02-04'} end={'2021-02-04'} />
-
-// <div
-// style={{
-//   display: 'flex',
-//   flexDirection: 'row',
-//   justifyContent: 'flex-end',
-// }}
-// >
-// <ReferralButton
-//   origin={originId}
-//   destination={destinationId}
-//   outboundPartialDate={outboundPartialDate}
-//   associateId={associateId}
-// />
-// </div>
-
-// <Typography>
-//   Quote age: {parseTime({ timestamp: quotedAt }).formattedDateTime}
-// </Typography>
-
-// <Typography>Price: {cost.formatted}</Typography>
-
-// <Typography>
-//   {parseTime({ timestamp: departing }).formattedDate}
-// </Typography>
-
-// import React from 'react';
-// import { Grid, Paper, Typography } from '@material-ui/core';
-// import { FlightType } from '../../type';
-// import { parseTime } from '../../helpers/parseTime';
-// import { ReferralButton } from './ReferralButton';
-
-// export const Flight: React.FC<FlightType> = ({
-//   from,
-//   to,
-//   departing,
-//   carrier,
-//   cost,
-//   direct,
-//   quotedAt,
-// }: FlightType) => {
-//   if (!from || !to || !departing || !cost) {
-//     return <></>;
-//   }
-//   const originId = from.iataCode;
-//   const destinationId = to.iataCode;
-//   const outboundPartialDate = departing.slice(0, 10);
-//   // This is not a real associate id
-//   const associateId = 'letspopto';
-//   return (
-//     <>
-//       <Paper style={{ margin: '8px' }}>
-//         <Grid container direction="column" alignContent="stretch">
-//           <Grid item>
-//             <Typography>
-//               {parseTime({ timestamp: departing }).formattedDate}
-//             </Typography>
-//           </Grid>
-//           <Grid item>
-//             <Grid
-//               container
-//               direction="row"
-//               justify="space-evenly"
-//               alignItems="baseline"
-//               spacing={2}
-//             >
-//               <Grid item xs={3}>
-//                 <Grid container direction="column">
-//                   <Grid item>
-//                     <Typography variant="h2" gutterBottom>
-//                       {from.iataCode}
-//                     </Typography>
-//                   </Grid>
-//                   <Grid item>
-//                     <Typography variant="subtitle1">{from.cityName}</Typography>
-//                   </Grid>
-//                   <Grid item>
-//                     <Typography variant="overline">
-//                       {from.countryName}
-//                     </Typography>
-//                   </Grid>
-//                 </Grid>
-//               </Grid>
-
-//               <Grid item xs={3}>
-//                 <Grid container direction="column" alignItems="center">
-//                   <Grid item>
-//                     <Typography variant="subtitle2">1:05hr est</Typography>
-//                   </Grid>
-//                   <Grid item>
-//                     <div
-//                       style={{
-//                         width: '10em',
-//                         height: '1px',
-//                         borderBottom: '2px solid black',
-//                       }}
-//                     />
-//                   </Grid>
-//                   <Grid item>
-//                     <Typography variant="caption" style={{ color: 'green' }}>
-//                       {direct ? 'direct' : 'not direct'}
-//                     </Typography>
-//                   </Grid>
-//                 </Grid>
-//               </Grid>
-//               <Grid item>
-//                 <Grid container direction="column">
-//                   <Grid item>
-//                     <Typography variant="h2" gutterBottom>
-//                       {to.iataCode}
-//                     </Typography>
-//                   </Grid>
-//                   <Grid item>
-//                     <Typography variant="subtitle1">{to.cityName}</Typography>
-//                   </Grid>
-//                   <Grid item>
-//                     <Typography variant="overline">{to.countryName}</Typography>
-//                   </Grid>
-//                 </Grid>
-//               </Grid>
-//             </Grid>
-//           </Grid>
-//         </Grid>
-
-//         <Typography>
-//           Quote age: {parseTime({ timestamp: quotedAt }).formattedDateTime}
-//         </Typography>
-//         <Typography>Price: {cost.formatted}</Typography>
-//         <div
-//           style={{
-//             display: 'flex',
-//             flexDirection: 'row',
-//             justifyContent: 'flex-end',
-//           }}
-//         >
-//           <ReferralButton
-//             origin={originId}
-//             destination={destinationId}
-//             outboundPartialDate={outboundPartialDate}
-//             associateId={associateId}
-//           />
-//         </div>
-//       </Paper>
-//       {/* <div>
-//         <pre>{JSON.stringify(from)}</pre>
-//         <pre>{JSON.stringify(to)}</pre>
-//         <pre>{JSON.stringify(departing)}</pre>
-//         <pre>{JSON.stringify(carrier)}</pre>
-//         <pre>{JSON.stringify(cost)}</pre>
-//         <pre>{JSON.stringify(direct)}</pre>
-//         <pre>{JSON.stringify(quotedAt)}</pre>
-//       </div> */}
-//     </>
-//   );
-// };
--- src/features/flights/FlightDestinations.tsx
+++ src/features/flights/FlightDestinations.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Grid, Typography } from '@material-ui/core';
 import { DestinationDivider } from './DestinationDivider';
 import './FlightDestinations.css';
 
@@ -43,73 +42,3 @@ export const FlightDestinations = ({
     </div>
   );
 };
-
-// export const FlightDestinations = ({
-//   fromIata,
-//   fromCityName,
-//   fromCountryName,
-//   toIata,
-//   toCityName,
-//   toCountryName,
-// }: {
-//   fromIata: string;
-//   fromCityName: string;
-//   fromCountryName: string;
-//   toIata: string;
-//   toCityName: string;
-//   toCountryName: string;
-// }) => {
-//   return (
-//     <Grid item>
-//       <Grid
-//         container
-//         direction="row"
-//         justify="space-between"
-//         alignItems="baseline"
-//         spacing={2}
-//       >
-//         <Grid item>
-//           <Grid container direction="column">
-//             <Grid item>
-//               <Typography variant="h2" gutterBottom>
-//                 {fromIata}
-//               </Typography>
-//             </Grid>
-//             <Grid item xs={3}>
-//               <Typography variant="subtitle1">{fromCityName}</Typography>
-//             </Grid>
-//             <Grid item>
-//               <Typography variant="overline">{fromCountryName}</Typography>
-//             </Grid>
-//           </Grid>
-//         </Grid>
-//         <Grid item>
-//           <div
-//             style={{
-//               height: '2px',
-//               minWidth: '50px',
-//               width: '100%',
-//               background: '#000000',
-//               transform: 'translateY(-10px)',
-//             }}
-//           />
-//         </Grid>
-//         <Grid item xs={3}>
-//           <Grid container direction="column">
-//             <Grid item>
-//               <Typography variant="h2" gutterBottom>
-//                 {toIata}
-//               </Typography>
-//             </Grid>
-//             <Grid item>
-//               <Typography variant="subtitle1">{toCityName}</Typography>
-//             </Grid>
-//             <Grid item>
-//               <Typography variant="overline">{toCountryName}</Typography>
-//             </Grid>
-//           </Grid>
-//         </Grid>
-//       </Grid>
-//     </Grid>
-//   );
-// };
