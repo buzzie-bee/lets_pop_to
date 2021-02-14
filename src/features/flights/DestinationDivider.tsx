81b5775d4b0aca5f837af5489603a30aab6ad7bb
buzzie-bee <39500959+buzzie-bee@users.noreply.github.com>
2021-02-14 19:36:10 +0100

    Updated destination card components to use makeStyles instead of css files for consistency

--- src/features/flights/DestinationDivider.tsx
+++ src/features/flights/DestinationDivider.tsx
@@ -1,26 +1,78 @@
-import './DestinationDivider.css';
+import { makeStyles } from '@material-ui/core';
 
 export const DestinationDivider = ({ stops }: { stops: number }) => {
+  const classes = useStyles();
+
   const renderStops = () => {
     if (stops === 0) {
       return;
     } else if (stops === 1) {
-      return <div className="dividerStop" />;
+      return <div className={classes.dividerStop} />;
     } else if (stops === 2) {
       return (
         <>
-          <div className="dividerStop d33" />
-          <div className="dividerStop d66" />
+          <div className={`${classes.dividerStop} ${classes.d33}`} />
+          <div className={`${classes.dividerStop} ${classes.d66}`} />
         </>
       );
     }
   };
 
   return (
-    <div className="destinationDivider">
-      <div className="dividerStart" />
+    <div className={classes.destinationDivider}>
+      <div className={classes.dividerStart} />
       {renderStops()}
-      <div className="dividerEnd" />
+      <div className={classes.dividerEnd} />
     </div>
   );
 };
+
+const useStyles = makeStyles((theme) => ({
+  destinationDivider: {
+    position: 'relative',
+    display: 'block',
+    height: '2px',
+    marginTop: '20px',
+    marginLeft: '50px',
+    marginRight: '60px',
+    width: '80%',
+    borderTop: '2px dashed #9933cc',
+  },
+  dividerStart: {
+    display: 'block',
+    position: 'absolute',
+    left: '-6px',
+    top: '-6px',
+    height: '12px',
+    width: '12px',
+    background: '#9933cc',
+    borderRadius: '50%',
+  },
+  dividerStop: {
+    display: 'block',
+    position: 'absolute',
+    left: '50%',
+    marginLeft: '-6px',
+    top: '-6px',
+    height: '12px',
+    width: '12px',
+    background: '#cf0000',
+    borderRadius: '50%',
+  },
+  d33: {
+    left: '33%',
+  },
+  d66: {
+    left: '66%',
+  },
+  dividerEnd: {
+    display: 'block',
+    position: 'absolute',
+    right: '-6px',
+    top: '-6px',
+    height: '12px',
+    width: '12px',
+    background: '#9933cc',
+    borderRadius: '50%',
+  },
+}));
--- src/features/flights/FlightDestinations.tsx
+++ src/features/flights/FlightDestinations.tsx
@@ -1,6 +1,6 @@
+import { makeStyles } from '@material-ui/core';
 import React from 'react';
 import { DestinationDivider } from './DestinationDivider';
-import './FlightDestinations.css';
 
 export const FlightDestinations = ({
   fromIata,
@@ -19,26 +19,86 @@ export const FlightDestinations = ({
   toCountryName: string;
   direct: boolean;
 }) => {
+  const classes = useStyles();
+
   return (
-    <div className="flightDestinationsContainer">
-      <div className="destinationContainer">
-        <div className="iata">{fromIata}</div>
-        <div className="city">{fromCityName}</div>
-        <div className="country">{fromCountryName}</div>
+    <div className={classes.flightDestinationsContainer}>
+      <div className={classes.destinationContainer}>
+        <div className={classes.iata}>{fromIata}</div>
+        <div className={classes.city}>{fromCityName}</div>
+        <div className={classes.country}>{fromCountryName}</div>
       </div>
-      <div className="dividerAndDirectContainer">
+      <div className={classes.dividerAndDirectContainer}>
         <DestinationDivider stops={direct ? 0 : 1} />
         {direct ? (
-          <div className="direct">Direct</div>
+          <div className={classes.direct}>Direct</div>
         ) : (
-          <div className="stops">Stops</div>
+          <div className={classes.stops}>Stops</div>
         )}
       </div>
-      <div className="destinationContainer">
-        <div className="iata">{toIata}</div>
-        <div className="city">{toCityName}</div>
-        <div className="country">{toCountryName}</div>
+      <div className={classes.destinationContainer}>
+        <div className={classes.iata}>{toIata}</div>
+        <div className={classes.city}>{toCityName}</div>
+        <div className={classes.country}>{toCountryName}</div>
       </div>
     </div>
   );
 };
+
+const useStyles = makeStyles((theme) => ({
+  flightDestinationsContainer: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+  },
+  destinationContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    minWidth: '125px',
+    maxWidth: '200px',
+  },
+  iata: {
+    fontSize: '3.5em',
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    lineHeight: '1em',
+  },
+  city: {
+    fontSize: '1.5em',
+    fontWeight: 200,
+    fontFamily: "Roboto', 'Helvetica', 'Arial', sans-serif",
+    lineHeight: '1em',
+  },
+  country: {
+    fontSize: '1em',
+    fontFamily: "Roboto', 'Helvetica', 'Arial', sans-serif",
+    fontWeight: 300,
+    lineHeight: '1.5em',
+  },
+  dividerAndDirectContainer: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  direct: {
+    fontFamily: "Roboto', 'Helvetica', 'Arial', sans-serif",
+    fontWeight: 500,
+    fontSize: '1rem',
+    lineHeight: '2.2',
+    marginLeft: '-6px',
+    color: '#00a640',
+  },
+  stops: {
+    fontFamily: "Roboto', 'Helvetica', 'Arial', sans-serif",
+    fontWeight: 500,
+    fontSize: '1rem',
+    lineHeight: '2.2',
+    marginLeft: '-6px',
+    color: '#a60000',
+  },
+}));
