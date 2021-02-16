9b8447b754d24439555603852e55e9e3b5f725a9
buzzie-bee <39500959+buzzie-bee@users.noreply.github.com>
2021-02-16 01:21:31 +0100

    Removed unused links for now

--- src/features/navBar/NavBar.tsx
+++ src/features/navBar/NavBar.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
-import { AppBar, Button, Link, makeStyles, Toolbar } from '@material-ui/core';
+import { AppBar, Link, makeStyles, Toolbar } from '@material-ui/core';
 import { ROUTES } from '../../constants/routes';
 
 export const NavBar: React.FC = () => {
   const classes = useStyles();
 
   return (
-    <AppBar
-      position="static"
-      // color="default"
-      elevation={0}
-      className={classes.appBar}
-    >
+    <AppBar position="static" elevation={0} className={classes.appBar}>
       <Toolbar>
         <Link
           component={RouterLink}
@@ -36,46 +31,14 @@ export const NavBar: React.FC = () => {
           >
             About
           </Link>
-          <Link
-            variant="button"
-            color="textPrimary"
-            href="#"
-            className={classes.link}
-          >
-            Link
-          </Link>
-          <Link
-            variant="button"
-            color="textPrimary"
-            href="#"
-            className={classes.link}
-          >
-            Link
-          </Link>
         </nav>
-        <Button
-          href="#"
-          color="inherit"
-          variant="outlined"
-          className={classes.link}
-        >
-          Login
-        </Button>
       </Toolbar>
     </AppBar>
   );
 };
 
 const useStyles = makeStyles((theme) => ({
-  '@global': {
-    ul: {
-      margin: 0,
-      padding: 0,
-      listStyle: 'none',
-    },
-  },
   appBar: {
-    borderBottom: `2px solid ${theme.palette.divider}`,
     background:
       'linear-gradient(90deg, rgba(95,11,136,1) 0%, rgba(153,51,204,1) 50%, rgba(191,83,245,1) 100%)',
     height: '5em',
@@ -88,6 +51,6 @@ const useStyles = makeStyles((theme) => ({
   },
   link: {
     margin: theme.spacing(1, 1.5),
-    color: '#fff',
+    color: '#FFFFFF',
   },
 }));
